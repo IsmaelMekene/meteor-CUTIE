53c0490cd116a35bf135f262ee5d4c124fa921a7
Mékéné <64429720+IsmaelMekene@users.noreply.github.com>
2021-03-06 03:05:16 +0100

    Update data_manipulation.py

--- data/data_manipulation.py
+++ data/data_manipulation.py
@@ -1,3 +1,11 @@
+'''
+@Author: Mékéné
+@Github: https://github.com/IsmaelMekene
+@Project: https://github.com/luyanger1799/meteor-CUTIE
+'''
+
+
+
 import numpy as np
 import pandas as pd 
 import glob2
